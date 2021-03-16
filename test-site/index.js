e6931aa95a5992fa1ea55527da0cad622bd82878
abeldemiss <69866709+abeldemiss@users.noreply.github.com>
2021-03-16 17:54:29 -0400

    delete

--- test-site/index.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav").style.padding = "10px 80px";
-  } else {
-    document.getElementById("nav").style.padding = "50px 80px";
-  }
-} 
